fd34a8779de6143374cae7b4609405a65f3aa2f7
mrdog233o5 <mr.dog233o5@gmail.com>
2024-04-09 13:19:54 +0800

    idk

--- main.js
+++ main.js
@@ -1,124 +1,95 @@
-const inputArea = document.getElementById('input');
-const outputDiv = document.getElementById('output');
-const convertButton = document.getElementById('convert');
-const modeSelect = document.getElementById('mode');
-
-
-const map = {
-    '`': '`',
-    '~': '`',
-    '1': '¡',
-    '!': '⁄',
-    '2': '™',
-    '@': '€',
-    '3': '£',
-    '#': '‹',
-    '4': '¢',
-    '$': '›',
-    '5': '∞',
-    '%': 'ﬁ',
-    '6': '§',
-    '^': 'ﬂ',
-    '7': '¶',
-    '&': '‡',
-    '8': '•',
-    '*': '°',
-    '9': 'ª',
-    '(': '·',
-    '0': 'º',
-    ')': '‚',
-    '-': '−',
-    '_': '—',
-    '=': '≠',
-    '+': '±',
-    'q': 'œ',
-    'Q': 'Œ',
-    'w': '∑',
-    'W': '„',
-    'e': '´',
-    'E': '´',
-    'r': '®',
-    'R': '‰',
-    't': '†',
-    'T': 'ˇ',
-    'y': '¥',
-    'Y': 'Á',
-    'u': '¨',
-    'U': '¨',
-    'i': 'ˆ',
-    'I': 'ˆ',
-    'o': 'ø',
-    'O': 'Ø',
-    'p': 'π',
-    'P': '∏',
-    '[': '“',
-    '{': '”',
-    ']': '‘',
-    '}': '’',
-    '\\': '«',
-    '|': '»',
-    'a': 'å',
-    'A': 'Å',
-    's': 'ß',
-    'S': 'Í',
-    'd': '∂',
-    'D': 'Î',
-    'f': 'ƒ',
-    'F': 'Ï',
-    'g': '©',
-    'G': '˝',
-    'h': '˙',
-    'H': '',
-    'j': '∆',
-    'J': 'Ô',
-    'k': '˚',
-    'K': '',
-    'l': '…',
-    'L': 'Ò',
-    ';': '…',
-    ':': 'Ú',
-    'z': 'Ω',
-    'Z': '¸',
-    'x': '≈',
-    'X': '˛',
-    'c': 'ç',
-    'C': 'Ç',
-    'v': '√',
-    'V': '◊',
-    'b': '∫',
-    'B': 'ı',
-    'n': '˜',
-    'N': '˜',
-    'm': 'µ',
-    'M': 'Â',
-    ',': '≤',
-    '<': '≤',
-    '.': '≥',
-    '>': '˘',
-    '/': '÷',
-    '?': '¿'
-};
-
-convertButton.addEventListener('click', () => {
-    const mode = modeSelect.value;
-    const inputText = inputArea.value;
-    let outputText = '';
-
-    if (mode === 'normal') {
-        outputText = convertNormal(inputText, map);
-    } else if (mode === 'reverse') {
-        outputText = convertReverse(inputText, map);
-    }
-
-    outputDiv.textContent = outputText;
-});
-
-function convertNormal(data, map) {
-    return data.split('').map(char => {
-        return Object.keys(map).find(key => map[key] === char) || char;
-    }).join('');
-}
-
-function convertReverse(data, map) {
-    return data.split('').map(char => map[char] || char).join('');
-}
+const match = `\`\`
+1¡
+2™
+3£
+4¢
+5∞
+6§
+7¶
+8•
+9ª
+0º
+-–
+=≠
+qœ
+w∑
+e´
+r®
+t†
+y¥
+u¨
+iˆ
+oø
+pπ
+[“
+]‘
+\\«
+aå
+sß
+d∂
+fƒ
+g©
+h˙
+j∆
+k˚
+l¬
+;…
+'æ
+zΩ
+x≈
+cç
+v√
+b∫
+n˜
+mµ
+,≤
+.≥
+/÷
+~~
+!⁄
+@€
+#‹
+$›
+%ﬁ
+^ﬂ
+&‡
+*°
+(·
+)‚
+QŒ
+W„
+E´
+R‰
+Tˇ
+YÁ
+U¨
+Iˆ
+OØ
+P∏
+{”
+}’
+|»
+AÅ
+SÍ
+DÎ
+FÏ
+G˝
+HÓ
+JÔ
+K
+LÒ
+:Ú
+"Æ
+Z¸
+X˛
+CÇ
+V◊
+Bı
+N˜
+MÂ
+<¯
+>˘
+?¿`.split("\n");
+match.forEach((idk) => {
+    console.log(idk);
+})
