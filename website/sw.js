69d890e2772426c414308c5bec09bd477af59668
littleblack111 <littleblack11111@gmail.com>
2024-04-11 23:11:01 +0800

    made it pwa

---
+++ website/sw.js
@@ -0,0 +1,18 @@
+// Cached core static resources 
+self.addEventListener("install",e=>{
+    e.waitUntil(
+      caches.open("static").then(cache=>{
+        return cache.addAll(["./",'./favicon.png', './index.html', './main.js', './vanilla-tilt.min.js', './style.css']);
+      })
+    );
+  });
+  
+  // Fatch resources
+  self.addEventListener("fetch",e=>{
+    e.respondWith(
+      caches.match(e.request).then(response=>{
+        return response||fetch(e.request);
+      })
+    );
+  });
+  
